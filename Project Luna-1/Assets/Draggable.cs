3f82afb67536c35f53cb7d1f7bf5dc52f7564c10
Atahan Yılmaz <ayilmaz16@ku.edu.tr>
2021-04-02 18:33:48 +0300

    Simple UI
    Drag and drop simple

---
+++ Project Luna-1/Assets/Draggable.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector3 oldPos;
+    private CanvasGroup canvasGroup;
+    public FramePiece CurrentFrame;
+
+    public bool SuccesfulDrop = false;
+
+    private void Awake() {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
+        CurrentFrame.DraggableLeft();
+        oldPos = this.transform.position;
+        Debug.Log("Begin drag: "+eventData.position+ ": "+this.transform.name);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+
+        this.transform.position = new Vector3(eventData.position.x,eventData.position.y,this.transform.position.z);
+    }
+
+    public void setFrameParent(FramePiece fp) {
+        SuccesfulDrop = true;
+        CurrentFrame = fp;
+        oldPos = CurrentFrame.transform.position;
+        
+
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        StartCoroutine(CheckValidDrop());
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    private IEnumerator CheckValidDrop() {
+        yield return new WaitForSeconds(0.05f);
+        if (SuccesfulDrop) {
+            SuccesfulDrop = false;
+            
+        } else {
+
+        }
+
+        this.transform.position = oldPos;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project Luna-1/Assets/FramePiece.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FramePiece : MonoBehaviour, IDropHandler
+{
+    private bool IsFree=true;
+
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log("On drop event!");
+        if (eventData.pointerDrag != null) {
+            //eventData.pointerDrag.GetComponent<Transform>().position = this.transform.position;
+            var drgbl = eventData.pointerDrag.GetComponent<Draggable>();
+            Debug.Log(drgbl);
+            if (drgbl!=null) {
+
+                if (IsFree) {
+                    IsFree = false;
+                    Debug.Log("Setting parent "+this.transform.name);
+                    drgbl.setFrameParent(this);
+                }
+
+            }
+        }
+    }
+
+    public void DraggableLeft() {
+        IsFree = true;
+    }
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
